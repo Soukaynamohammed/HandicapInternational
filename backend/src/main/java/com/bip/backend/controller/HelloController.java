2dc192db1ba629857f9338d5fb6fddf9d2499848
Name <email@email.com>
2024-03-31 17:38:23 +0200

    Adding database and basic test Repository

--- backend/src/main/java/com/bip/backend/controller/HelloController.java
+++ backend/src/main/java/com/bip/backend/controller/HelloController.java
@@ -1,13 +1,30 @@
 package com.bip.backend.controller;
 
+import com.bip.backend.entity.Message;
+import com.bip.backend.repository.MessageRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private MessageRepository messageRepository;
+
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, World!";
+        List<Message> messages = messageRepository.findAll();
+        if (messages.isEmpty()) {
+            return "Hello, World! No entries found";
+        } else {
+            return messages.stream()
+                    .map(Message::getContent)
+                    .collect(Collectors.joining("\n"));
+        }
     }
 }

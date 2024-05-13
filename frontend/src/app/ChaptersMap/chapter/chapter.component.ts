efa59bb3442cb556d68ce36382b0cbb16a14497c
Soukaynamohammed <soukaynamohammmed@gmail.com>
2024-05-13 16:16:19 +0200

    koptekst stylen chapters

--- frontend/src/app/ChaptersMap/chapter/chapter.component.ts
+++ frontend/src/app/ChaptersMap/chapter/chapter.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { NavbarComponent } from '../../navbar/navbar.component';
 import { GameComponent } from '../game/game.component';
 import { LessenComponent } from '../lessen/lessen.component'
+import { ChaptersService } from '../../Services/chapters.service';
+import {Chapter} from  '../../Services/chapters.service';
 
 @Component({
   selector: 'app-chapter',
@@ -10,17 +12,24 @@ import { LessenComponent } from '../lessen/lessen.component'
   styleUrl: './chapter.component.scss'
 })
 export class ChapterComponent implements OnInit {
-  chapterId: number = 0 ;
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router,private chapterService: ChaptersService) { }
+
+  chapterId: number = 0 ;
+  chapters: Chapter[] =[];
 
   ngOnInit(): void {
     this.chapterId = +this.route.snapshot?.paramMap.get('id')!;
+    this.fetchChapter()
   }
   
   navigateToQuiz(): void {
     this.router.navigate(['/quiz', this.chapterId]);
   }
 
-
+  fetchChapter():void{
+    this.chapterService.getAllChapters().subscribe(chapters =>{
+      this.chapters = chapters;
+    })
+  }
 }
